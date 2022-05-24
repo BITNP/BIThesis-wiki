b251109acfebf1980bae9d2d45e9b8e12585acb6
Feng Kaiyu <loveress01@outlook.com>
2022-05-24 11:34:45 +0800

    update new-wiki.

--- new-wiki/src/pages/index.tsx
+++ new-wiki/src/pages/index.tsx
@@ -5,19 +5,20 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import bithesis from '@site/static/img/bithesis.png';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <img width="60%" src={bithesis} alt="bithesis" />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/faq">
-            开始
+            常见问题
           </Link>
         </div>
       </div>
